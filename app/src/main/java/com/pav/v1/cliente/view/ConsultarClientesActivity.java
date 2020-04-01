17a00ba0c17aa1385f93335a7b439f73a7cc60db
vieirapav <62731148+vieirapav@users.noreply.github.com>
2020-04-01 15:19:23 -0300

    V1.1

---
+++ app/src/main/java/com/pav/v1/cliente/view/ConsultarClientesActivity.java
@@ -0,0 +1,14 @@
+package com.pav.v1.cliente.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultarClientesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_clientes);
+    }
+}
