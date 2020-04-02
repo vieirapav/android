3d420bb069e952b2deeaa0c676f2e3441d429f1a
vieirapav <62731148+vieirapav@users.noreply.github.com>
2020-04-02 11:07:18 -0300

    V1.erro

---
+++ app/src/main/java/com/pav/v1/cliente/view/linha_detalhe.java
@@ -0,0 +1,16 @@
+package com.pav.v1.cliente.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pav.v1.cliente.R;
+
+public class linha_detalhe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linha_detalhe);
+    }
+}
